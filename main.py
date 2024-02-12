d6ff650360afd30925f05601f3f7592f896ea928
Peter Huynh <peterhyh@yahoo.com>
2024-02-12 13:14:35 -0600

    random number test

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import phonenumbers
 from phonenumbers import geocoder, carrier
 
-x = phonenumbers.parse("+14698319568", None)
+x = phonenumbers.parse("+16824180392", None)
 
 caller_loc = geocoder.description_for_number(x, "us")
 caller_carrier = carrier.name_for_number(x, "en")
