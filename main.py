3ea230b87a101d82fc679e1858c59d082bca61b2
Peter Huynh <peterhyh@yahoo.com>
2024-02-15 18:18:11 -0600

    button logic implemented

--- main.py
+++ main.py
@@ -2,29 +2,29 @@ import phonenumbers
 from phonenumbers import geocoder, carrier, timezone
 from tkinter import *
 
-number = "213 244 1430"
-
-parsed_number = phonenumbers.parse(number, "US")
-
 
 def find_user():
+    number = user_input.get()
+
+    parsed_number = phonenumbers.parse(number, "US")
+
     if phonenumbers.is_possible_number(parsed_number):
         if phonenumbers.is_valid_number(parsed_number):
             caller_location = geocoder.description_for_number(
                 parsed_number, "en")
             caller_carrier = carrier.name_for_number(parsed_number, "en")
             caller_timezone = timezone.time_zones_for_number(parsed_number)
-            print(parsed_number)
-            print(caller_location)
-            print(caller_carrier)
-            print(caller_timezone)
         else:
             print("NPA 200 not used")
     else:
         print("invalid number")
 
+    output.config(
+        text=f"Location: {caller_location}\nPhone Carrier: {caller_carrier}\nTime Zone: {caller_timezone}")
+
 
 # UI ----------------------------
+
 window = Tk()
 window.title("Phone Number Locator")
 window.minsize(width=500, height=500)
@@ -38,5 +38,9 @@ user_input.grid(column=2, row=0)
 
 button = Button(text="Search", command=find_user)
 button.grid(column=2, row=2)
+
+output = Label(text=f"")
+output.grid(column=2, row=3)
+
 
 window.mainloop()
